4cd5a16858c9d1c2657d4e8d486796f376de1429
SHV <shvcodes@gmail.com>
2023-08-09 11:14:31 +0530

    Commit

---
+++ LectureCodes/src/RecusrionFAANGQuestions/KeypadCombination.java
@@ -0,0 +1,32 @@
+package RecusrionFAANGQuestions;
+
+import java.util.ArrayList;
+
+public class KeypadCombination {
+    public static void main(String[] args) {
+        System.out.println(pad("","12"));
+
+    }
+
+    static ArrayList<String> pad(String p, String up){
+
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = up.charAt(0) -'0'; //This will convert '2' into 2
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit-1)*3; i < digit*3; i++) {
+
+            char ch = (char)('a' + i);
+            list.addAll(pad(p+ch,up.substring(1)));
+
+        }
+        return list;
+
+    }
+}
